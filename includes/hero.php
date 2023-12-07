4057176f15f9ccea8bc54f4d711003f06de0a27f
Truongngocanh <ngocanh193210@gmail.com>
2023-12-08 00:14:28 +0700

    add file

---
+++ includes/hero.php
@@ -0,0 +1,54 @@
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="hero__categories">
+                        <div class="hero__categories__all">
+                            <i class="fa fa-bars"></i>
+                            <span>Danh mục sản phẩm</span>
+                        </div>
+                        <ul>
+                            <li><a href="#">Sữa rửa mặt</a></li>
+                            <li><a href="#">Serum</a></li>
+                            <li><a href="#">Toner</a></li>
+                            <li><a href="#">Toner</a></li>
+                            <li><a href="#">Toner</a></li>
+                            <li><a href="#">Toner</a></li>
+                            <li><a href="#">Toner</a></li>
+                            <li><a href="#">Toner</a></li>
+                            <li><a href="#">Toner</a></li>
+                            <li><a href="#">Toner</a></li>
+                            <li><a href="#">Toner</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="hero__search">
+                        <div class="hero__search__form">
+                            <form action="#">
+                                <input type="text" placeholder="Tìm kiếm...">
+                                <button type="submit" class="site-btn">Search</button>
+                            </form>
+                        </div>
+                        <div class="hero__search__phone">
+                            <div class="hero__search__phone__icon">
+                                <i class="fa fa-phone"></i>
+                            </div>
+                            <div class="hero__search__phone__text">
+                                <h5>+96 999 9999</h5>
+                                <span>hỗ trợ 24/7</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hero__item set-bg" data-setbg="img/hero/skincare-tips.jpg">
+                        <div class="hero__text">
+                            <span>skincare</span>
+                            <h2>Làm đẹp da <br />100% tự nhiên</h2>
+                            <p>Giao hàng nhanh - miễn phí</p>
+                            <a href="shop-grid.php" class="primary-btn">SHOP NOW</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
